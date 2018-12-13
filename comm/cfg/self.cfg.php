1493147bc7be75e0c6d0127db5b0229ae0f0022b
messge4net <message4net@outlook.com>
2018-12-13 18:04:03 +0800

    dfafsdf

--- comm/cfg/self.cfg.php
+++ comm/cfg/self.cfg.php
@@ -1,13 +1,14 @@
 <?php
 //定义起始URL
 //define('URL_REL', 'http://127.0.0.1:8888/youn9php/');
-define('URL_BASE', 'http://www.yonderwon.com:8888/yonderwon/');
+//define('URL_BASE', 'http://www.yonderwon.com:8888/yonderwon/');
+define('URL_BASE', 'http://www.yonderwon.com/yonderwon/');
 //define('URL_ALIS','');
 //define('URL_ALIS','public/');
 
 //定义数据库
-define('DB_NAME','ydv');
+define('DB_NAME','yd_ops');
 define('DB_HOST','127.0.0.1');
-define('DB_PORT','8889');
-define('DB_USER','root');
-define('DB_PASSWD','root');
+define('DB_PORT','3306');
+define('DB_USER','proapp');
+define('DB_PASSWD','proapp_@liujin.1243.com');
